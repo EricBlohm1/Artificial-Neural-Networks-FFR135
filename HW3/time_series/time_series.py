5fde5e48a97d70b91f748d677ab182edc92d740b
EricBlohm1 <89911464+EricBlohm1@users.noreply.github.com>
2024-10-12 18:36:35 +0200

    dummy text

--- HW3/time_series/time_series.py
+++ HW3/time_series/time_series.py
@@ -18,11 +18,13 @@ def init_w_r(M):
 
 
 def main():
+    #### Retrieve data ####
     df = pd.read_csv('training-set.csv', header=None)
     x = df.to_numpy()
 
     test_df = pd.read_csv('test-set-9.csv',header=None)
     test_x = test_df.to_numpy()
+    #######################
 
     ### Init ###
     #input neurons
@@ -36,8 +38,8 @@ def main():
     predict_T = 500
     ############
 
+    #### Calculate the reservoir using the input ####
     R = np.zeros((T,M))
-    #create the R matrix
     for t in range(0,T-1):
         r_next = np.zeros(M)
         r_t = R[t].copy()
@@ -46,25 +48,29 @@ def main():
 
         r_next = np.tanh(sum1+sum2)
         R[t+1] = r_next
+    #################################################
 
+    #### Compute the output weights using ridge regression ###
     I = np.identity(M)
     #X had each column as a time step, R has each row as a time step. Transpose x in order to make the calculation correct.
     x=x.T
     inverse = np.linalg.inv(np.dot(R.T,R) + k * I)
     dot1 = np.dot(inverse,R.T)
     w_out = np.dot(dot1,x) 
+    ##########################################################
 
-    #Calculate output
+    #### Calculate output ####
     O = np.zeros((T,N))
     for t in range(0,T-1):
         O[t+1] = np.dot(w_out.T,R[t+1])
+    ##########################
     
 
-    #calculate new R using test data and new function.
+    #### Calculate new R using test data and then using the output. ####
     R_predict = np.zeros((len(test_x[0])+predict_T,M))
     O_predict = np.zeros((len(test_x[0])+predict_T,N))
 
-    #First create the R matrix and output using the 100 values from the test data
+    # First create the R matrix and output using the 100 values from the test data
     # then continue to predict these values from the test data
     for t in range(0,len(test_x[0])+predict_T-1):
         r_next = np.zeros(M)
@@ -80,13 +86,17 @@ def main():
 
         R_predict[t+1] = r_next
         O_predict[t+1] = np.dot(w_out.T,R_predict[t+1])
+    #######################################################################
 
+    ### Save answer, y-component of the output ###
     O_predict = O_predict.T
+    # slice off the 100 first elements, coming from the test data, not prediction.
     O_predict = O_predict[:, 100:]
     print(O_predict)
     answer = O_predict[1]
     df = pd.DataFrame([answer])
     df.to_csv('prediction.csv', index=False, header=False)
+    ##############################################
 
 
 
